ccee05b0db82a9844750d2fc8a873a689fc4d367
Md Sameem <mohammadsameem559@gmail.com>
2026-06-28 19:02:00 +0530

    cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://tasktracker-opal.vercel.app",
     credentials: true,
 }));
 
